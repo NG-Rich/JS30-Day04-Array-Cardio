047d094f8862005b54684f2e863446aef7bfabdd
Richard Chan <rchan817@yahoo.com>
2020-11-10 15:37:34 -0800

    added html and js files along with stylesheet

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const inventors = [
   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 
+const people = ['Blake, William', 'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 
                 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 
                 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 
                 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 
@@ -26,33 +26,211 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel',
                 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 
                 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 
                 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 
-                'Blair, Tony', 'Blake, William'];
+                'Blair, Tony'];
 
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
-
-// Array.prototype.map()
-// 2. Give us an array of the inventors first and last names
-
-// Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
-
-// Array.prototype.reduce()
-// 4. How many years did all the inventors live all together?
-
-// 5. Sort the inventors by years lived
-
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-
-
-// 7. sort Exercise
-// Sort the people alphabetically by last name
-
-// 8. Reduce Exercise
-// Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 
               'truck', 'bike', 'walk', 
               'car', 'van', 'bike', 
               'walk', 'car', 'van', 
               'car', 'truck' ];
+
+// Shows list of inventors          
+const showInventors = () => {
+  const toggleInventors = document.getElementById("inventors");
+  document.getElementById("inventors").innerHTML = JSON.stringify(inventors, null, 4);
+  
+  if (toggleInventors.style.display === "inline-block") {
+    toggleInventors.style.display = "none";
+  }else {
+    toggleInventors.style.display = "inline-block";
+  }
+}
+
+// Shows list of people
+const showPeople = () => {
+  const togglePeople = document.getElementById("people");
+  document.getElementById("people").innerHTML = JSON.stringify(people, null, 4);
+  
+  if (togglePeople.style.display === "inline-block") {
+    togglePeople.style.display = "none";
+  }else {
+    togglePeople.style.display = "inline-block";
+  }
+}
+
+// Shows list of data
+const showData = () => {
+  const toggleData = document.getElementById("data");
+  document.getElementById("data").innerHTML = JSON.stringify(data, null, 4);
+    
+  if (toggleData.style.display === "inline-block") {
+    toggleData.style.display = "none";
+  }else {
+    toggleData.style.display = "inline-block";
+  }
+}
+
+// Array.prototype.filter()
+const inventorFilter = () => {
+  const oldInventors = [];
+  const toggleInventors = document.getElementById("inventorsFilter");
+
+  inventors.forEach(inventor => {
+    if (inventor.year >= 1500 && inventor.year < 1600) {
+      oldInventors.push(inventor);
+      document.getElementById("inventorsFilter").innerHTML = JSON.stringify(oldInventors, null, 4);
+    }
+  })
+
+  if (toggleInventors.style.display === "inline-block") {
+    toggleInventors.style.display = "none";
+  }else {
+    toggleInventors.style.display = "inline-block";
+  }
+}
+
+// Array.prototype.map()
+const inventorsMap = () => {
+  const toggleInventors = document.getElementById("inventorsMap");
+  const inventorsFirstLast = inventors.map(inventor => [inventor.first, inventor.last]);
+
+  document.getElementById("inventorsMap").innerHTML = JSON.stringify(inventorsFirstLast, null, 4);
+
+  if (toggleInventors.style.display === "inline-block") {
+    toggleInventors.style.display = "none";
+  }else {
+    toggleInventors.style.display = "inline-block";
+  }
+}
+
+// Array.prototype.sort()
+const inventorsSort = () => {
+  const inventorsBirthday = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+  const toggleInventors = document.getElementById("inventorsSort");
+  document.getElementById("inventorsSort").innerHTML = JSON.stringify(inventorsBirthday, null, 4);
+
+  if (toggleInventors.style.display === "inline-block") {
+    toggleInventors.style.display = "none";
+  }else {
+    toggleInventors.style.display = "inline-block";
+  }
+}
+
+// Array.prototype.reduce()
+const inventorsReduce = () => {
+  const inventorsTotalYears = inventors.reduce((total, inventor) => total + (inventor.passed - inventor.year), 0);
+  const toggleInventors = document.getElementById("inventorsReduce");
+  document.getElementById("inventorsReduce").innerHTML = "Total years: " + inventorsTotalYears;
+
+  if (toggleInventors.style.display === "flex") {
+    toggleInventors.style.display = "none";
+  }else {
+    toggleInventors.style.display = "flex";
+  }
+}
+
+// People sort Exercise
+const peopleSort = () => {
+  const sortPeople = people.sort((lastPerson, nextPerson) => {
+    const [aLast, aFirst] = lastPerson.split(", ");
+    const [bLast, bFirst] = nextPerson.split(", ");
+    return aLast > bLast ? 1 : -1;
+  });
+
+  const togglePeople = document.getElementById("peopleSort");
+  document.getElementById("peopleSort").innerHTML = JSON.stringify(sortPeople, null, 4);
+
+  if (togglePeople.style.display === "block") {
+    togglePeople.style.display = "none";
+  }else {
+    togglePeople.style.display = "block";
+  }
+}
+
+// 8. Reduce Exercise
+const dataReduce = () => {
+  const howManyInstances = data.reduce((obj, item) => {
+    if(!obj[item]) {
+      obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+  }, {});
+
+  const toggleData = document.getElementById("dataReduce");
+  document.getElementById("dataReduce").innerHTML = JSON.stringify(howManyInstances, null, 4);
+
+  if (toggleData.style.display === "inline-block") {
+    toggleData.style.display = "none";
+  }else {
+    toggleData.style.display = "inline-block";
+  }
+}
+
+/*
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+inventors.forEach(inventor => {
+  if (inventor.year >= 1500 && inventor.year < 1600) {
+    console.log(inventor);
+  }
+});
+
+const checkInventors = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.log(checkInventors);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const inventorsFirstLast = inventors.map(inventor => [inventor.first, inventor.last]);
+
+inventorsFirstLast.forEach(invent => {
+  console.log(invent);
+});
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const inventorsBirthday = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+
+console.log(inventorsBirthday);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+const inventorsTotalYears = inventors.reduce((total, inventor) => total + (inventor.passed - inventor.year), 0);
+
+console.log(inventorsTotalYears);
+
+// 5. Sort the inventors by years lived
+const inventorsBirthday = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+
+console.log(inventorsBirthday);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector(".mw-category");
+const links = Array.from(category.querySelectorAll("a"));
+const de = links.map(link => link.textContent).filter(streetName => streetName.includes("de"));
+
+const category = Array.from(document.querySelectorAll(".mw-category a"));
+const de = category.map(link => link.textContent).filter(streetName => streetName.includes("de"));
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const sortPeople = people.sort((lastPerson, nextPerson) => {
+  const [aLast, aFirst] = lastPerson.split(", ");
+  const [bLast, bFirst] = nextPerson.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
+console.log(sortPeople);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const howManyInstances = data.reduce((obj, item) => {
+  if(!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(howManyInstances);
+*/
